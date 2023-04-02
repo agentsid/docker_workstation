65f2f560536ca4b888b0effd10de1bc606111080
agentsid <siddharthsingh2910@gmail.com>
2023-04-03 00:46:13 +0530

    docker

--- find-ip.sh
+++ find-ip.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+rm -rf /home/siddharth/inventory/hosts
 echo "[docker_host]" >> /home/siddharth/inventory/hosts
 for i in  $(docker ps -q) ; do
-    echo "$i"
+    #echo "$i"
     docker exec -it $i  hostname -i >> /home/siddharth/inventory/hosts
 done
 cat /home/siddharth/inventory/hosts
